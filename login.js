f2194ba8204dd91f35fe9037a7c644a8ee3884a7
LeviBMG <geimerlevi@gmail.com>
2024-10-01 13:35:20 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+const users = {
+    'Max': 'PWvonMax',
+    'Levi': 'Banane14'
+};
+
+function login(username, password) {
+    if (users[username] && users[username] === password) {
+        localStorage.setItem('loggedInUser', username);
+        return true;
+    }
+    return false;
+}
+// Login-Formular-Event-Listener
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (users[username] && users[username] === password) {
+        // Speichern des Login-Status im localStorage
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'website.html';
+    } else {
+        alert('Ungültiger Benutzername oder Passwort (versuchs doch nochmal. Wenns dann immer noch nicht klappt hast du n schlechtes gedächtnis. LOL)');
+    }
+});
